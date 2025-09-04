c77620bcf7c67fc64cadd45cb4803a79d5d4ec25
abdelhakim allouani <abdelhakimallouani@gmail.com>
2025-09-04 18:23:37 +0200

    ex10

---
+++ youcode-sas-les-boucles2/chall13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<math.h>
+
+int main(){
+    int n, i, p ,s=0;
+    printf("enter un nombre entre n : ");
+    scanf("%d",&n);
+
+    for ( i = 1; i <= 10 ; i++)
+    {
+        p = n*i ;
+        s = s +p;
+        printf("%d*%d=%d\n", n,i,p);
+    }
+    printf("la sommes est : %d ", s);
+    return 0;
+}
