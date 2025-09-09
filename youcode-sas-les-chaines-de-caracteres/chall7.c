d5e74f422b3ce1c0bbc7c4c39a70d7649143d4c8
abdelhakim allouani <abdelhakimallouani@gmail.com>
2025-09-09 17:41:25 +0200

    ex6- toupper()

---
+++ youcode-sas-les-chaines-de-caracteres/chall7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char C[50];
+
+    printf("entrez votre chaine : ");
+    fgets(C, sizeof(C), stdin);
+    
+    for (int i = 0; i <= strlen(C); i++)
+    {
+        C[i] = toupper((char)C[i]); ; 
+    }
+    printf("la chaine en en majuscules : %s\n", C);
+
+    return 0;
+}
