e532ba88d846a42c5b6784059c130ecd7d2ee871
abdelhakim allouani <abdelhakimallouani@gmail.com>
2025-09-11 12:04:17 +0200

    ex8 l'air

---
+++ youcode-sas-les-structures/chall8.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct Cercle {
+    float rayon;
+}cercle;
+
+float Rcercle(cercle c) {
+    float const PI = 3.14;
+    return PI * c.rayon * c.rayon;
+}
+
+int main() {
+    cercle c;
+    printf("entrer le rayon du cercle: ");
+    scanf("%f", &c.rayon);
+
+    printf("l'aire du cercle est: %.2f\n", Rcercle(c));
+    return 0;
+}
